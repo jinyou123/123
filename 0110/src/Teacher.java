e866c812f3f449a8a9fe631c1043b4167edcbab4
Administrator <2470642801@qq.com>
2021-01-10 22:15:19 +0800

    2021-1-10

---
+++ 0110/src/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher extends User{
+
+    public Teacher(){
+        super("123456");
+    }
+    private String zhiCheng;
+
+    public String getZhiCheng() {
+        return zhiCheng;
+    }
+
+    public void setZhiCheng(String zhiCheng) {
+        this.zhiCheng = zhiCheng;
+    }
+}
